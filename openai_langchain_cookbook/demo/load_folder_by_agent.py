2bc8d4c733eb0bf371e7b420e0bb05bee4d4208a
kirin <kirin.chen1001@gmail.com>
2024-05-18 23:48:49 +0800

    upgrade new agent syntax

--- openai_langchain_cookbook/demo/load_folder_by_agent.py
+++ openai_langchain_cookbook/demo/load_folder_by_agent.py
@@ -1,12 +1,14 @@
 import os
-from langchain_community.agent_toolkits.load_tools import load_tools
+
+from langchain import hub
+from langchain.agents import initialize_agent, AgentType, create_structured_chat_agent, AgentExecutor, \
+    create_react_agent
+from langchain.chains import RetrievalQA
 from langchain_core.tools import Tool
+from langchain_openai import ChatOpenAI
 
 import main
-from utils import asset_utils, chroma_utils
-from langchain.chains import RetrievalQA
-from langchain_openai import OpenAI, ChatOpenAI
-from langchain.agents import initialize_agent, AgentType
+from utils import chroma_utils
 
 if __name__ == '__main__':
     main.init_base()
@@ -18,12 +20,7 @@ if __name__ == '__main__':
     llm = ChatOpenAI(model_name="gpt-4")
 
     # Construct a QA Chain to handle queries
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        return_source_documents=True
-    )
+
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever, return_source_documents=True,
                                         verbose=True, input_key="question")
 
@@ -34,22 +31,25 @@ if __name__ == '__main__':
     tools = [
         Tool(
             name="Project expert system",
-            func=lambda query: chain({"question": query}),
+            func=lambda query: chain.invoke({"question": query}),
             description="Relevant content or suggestions for modifications about the project.",
         ),
     ]
 
-    agent = initialize_agent(
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        tools=tools,
-        llm=llm,
-        verbose=True,  # Use verbose=True to check the process of choosing tool by Agent
-        handle_parsing_errors=True  # Added to handle parsing errors
-    )
+    # agent = initialize_agent(
+    #     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    #     tools=tools,
+    #     llm=llm,
+    #     verbose=True,  # Use verbose=True to check the process of choosing tool by Agent
+    #     handle_parsing_errors=True  # Added to handle parsing errors
+    # )
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, )
     query = "How to fix my project HelloWorld.main ? and show all fixed code."
 
     # query = "What Language in this project?"
-    result = agent.run(query)
+    result = agent_executor.invoke({"input": query})
     print(result)
     # print(result["result"])
     # query = "How to fix my project HelloWorld.main ?"
